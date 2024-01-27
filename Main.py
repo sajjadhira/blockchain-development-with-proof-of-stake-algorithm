1b1431b69dedeac6e1c54babbce13b90be4e6918
sajjadhira <sajjad.hira12@gmail.com>
2024-01-27 21:45:19 +0600

    Node initiated

--- Main.py
+++ Main.py
@@ -6,48 +6,10 @@ import pprint
 from Blockchain import Blockchain
 from BlockchainUtils import BlockchainUtils
 from AccountModel import AccountModel
+from Node import Node
 
 if __name__ == '__main__':
-
-    blockchain = Blockchain()
-    pool = TransactionPool()
-
-    alice = Wallet()
-    bob = Wallet()
-    exchange = Wallet()
-    forger = Wallet()
-
-    exchangeTransaction = exchange.createTransaction(
-        alice.publicKeyString(), 10, 'EXCHANGE')
-
-    if not pool.transactionExists(exchangeTransaction):
-        pool.addTransaction(exchangeTransaction)
-
-    coveredTransactions = blockchain.getCoveredTransactionSet(
-        pool.transactions)
-    lastHash = BlockchainUtils.hash(
-        blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-
-    blockOne = forger.createBlock(
-        coveredTransactions, lastHash, blockCount)
-    blockchain.addBlock(blockOne)
-    pool.removeTransaction(blockOne.transactions)
-    ''' Alice wants to send 5 Token to Bob '''
-    transaction = alice.createTransaction(bob.publicKeyString(), 5, 'TRANSFER')
-
-    if not pool.transactionExists(transaction):
-        pool.addTransaction(transaction)
-
-    coveredTransactions = blockchain.getCoveredTransactionSet(
-        pool.transactions)
-    lastHash = BlockchainUtils.hash(
-        blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-
-    blockTwo = forger.createBlock(
-        coveredTransactions, lastHash, blockCount)
-    blockchain.addBlock(blockTwo)
-    pool.removeTransaction(blockTwo.transactions)
-
-    pprint.pprint(blockchain.toJson())
+    node = Node()
+    print(node.blockchain)
+    print(node.wallet)
+    print(node.transactionPool)
