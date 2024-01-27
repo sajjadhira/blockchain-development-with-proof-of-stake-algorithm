add2f16f749f3b84428db1b964e83e7e20462d70
sajjadhira <sajjad.hira12@gmail.com>
2024-01-27 16:38:19 +0600

    Account Model Implimented

--- Main.py
+++ Main.py
@@ -5,38 +5,13 @@ from Block import Block
 import pprint
 from Blockchain import Blockchain
 from BlockchainUtils import BlockchainUtils
+from AccountModel import AccountModel
 
 if __name__ == '__main__':
-    sender = 'sender'
-    recipient = 'recipient'
-    amount = 100
-    type = 'TRANSFER'
 
     wallet = Wallet()
+    accountModel = AccountModel()
+    accountModel.updateBalance(wallet.publicKeyString(), 10)
+    accountModel.updateBalance(wallet.publicKeyString(), -5)
 
-    fruadulentWallet = Wallet()
-
-    pool = TransactionPool()
-
-    transaction = wallet.createTransaction(recipient, amount, type)
-
-    if not pool.existingTransaction(transaction):
-        pool.addTransaction(transaction)
-
-    blockchain = Blockchain()
-    lastHash = BlockchainUtils.hash(
-        blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-
-    block = wallet.createBlock(
-        pool.transactions, lastHash, wallet.publicKeyString(), blockCount)
-
-    if not blockchain.lastBlockHashValid(block):
-        print('Invalid block hash')
-
-    elif not blockchain.blockCountValid(block):
-        print('Invalid block count')
-    else:
-        blockchain.addBlock(block)
-
-    pprint.pprint(blockchain.toJson())
+    print(accountModel.balances)
