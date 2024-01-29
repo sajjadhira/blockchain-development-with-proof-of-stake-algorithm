1b2debe3b109b1aa6674cbd8558ddf4becbf1f2b
sajjadhira <sajjad.hira12@gmail.com>
2024-01-29 15:03:06 +0600

    Random Pickup Done

--- ProofOfStake.py
+++ ProofOfStake.py
@@ -1,3 +1,8 @@
+
+from Lot import Lot
+from BlockchainUtils import BlockchainUtils
+
+
 class ProofOfStake:
 
     def __init__(self):
@@ -18,5 +23,28 @@ class ProofOfStake:
         else:
             return None
 
-    def getStakers(self):
-        return self.stakers
+    def validatorLots(self, seed):
+        lots = []
+        for validator in self.stakers.keys():
+            for stake in range(self.get(validator)):
+                lots.append(Lot(validator, stake + 1, seed))
+
+        return lots
+
+    def winnerLot(self, lots, seed):
+        winnerLot = None
+        leastOffset = None
+        referenceHashIntValue = int(BlockchainUtils.hash(seed).hexdigest(), 16)
+        for lot in lots:
+            lotIntValue = int(lot.lotHash(), 16)
+            offset = abs(lotIntValue - referenceHashIntValue)
+            if leastOffset is None or offset < leastOffset:
+                leastOffset = offset
+                winnerLot = lot
+
+        return winnerLot
+
+    def forger(self, lastBlockHash):
+        lots = self.validatorLots(lastBlockHash)
+        winnerLot = self.winnerLot(lots, lastBlockHash)
+        return winnerLot.publicKey
--- Test.py
+++ Test.py
@@ -1,15 +1,27 @@
 from ProofOfStake import ProofOfStake
 from Lot import Lot
+import random
+import string
+
+
+def getRandomString(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
 
 if __name__ == '__main__':
-    # pos = ProofOfStake()
-    # pos.update('Bob', 10)
-    # pos.update('Alice', 100)
+    pos = ProofOfStake()
+    pos.update("bob", 10)
+    pos.update("alice", 100)
 
-    # print(pos.get('Bob'))
-    # print(pos.get('Alice'))
+    bobWins = 0
+    aliceWins = 0
 
-    # print(pos.get('Jack'))
-
-    lot = Lot('bob', 1, '00000000')
-    print(lot.lotHash())
+    for i in range(100):
+        forger = pos.forger(getRandomString(i))
+        if forger == "bob":
+            bobWins += 1
+        elif forger == "alice":
+            aliceWins += 1
+    print("Bob wins: " + str(bobWins) + " times \n\n")
+    print("Alice wins: " + str(aliceWins) + " times \n\n")
