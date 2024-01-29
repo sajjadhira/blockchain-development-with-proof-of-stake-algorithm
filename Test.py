bcdd263b54d06e287b9147b1b8bb68d7d6d3179a
sajjadhira <sajjad.hira12@gmail.com>
2024-01-29 14:21:53 +0600

    Lot and Lot Hash initiated

--- Test.py
+++ Test.py
@@ -1,11 +1,15 @@
 from ProofOfStake import ProofOfStake
+from Lot import Lot
 
 if __name__ == '__main__':
-    pos = ProofOfStake()
-    pos.update('Bob', 10)
-    pos.update('Alice', 100)
+    # pos = ProofOfStake()
+    # pos.update('Bob', 10)
+    # pos.update('Alice', 100)
 
-    print(pos.get('Bob'))
-    print(pos.get('Alice'))
+    # print(pos.get('Bob'))
+    # print(pos.get('Alice'))
 
-    print(pos.get('Jack'))
+    # print(pos.get('Jack'))
+
+    lot = Lot('bob', 1, '00000000')
+    print(lot.lotHash())
